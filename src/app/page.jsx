8fd0e0bac2f2545a80f9ee41eb856f4c5879991e
margofirsik <margofirsik@gmail.com>
2024-10-04 18:05:12 +0300

    DDED

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -35,9 +35,9 @@ const Home = () => {
 	}, []);
 
 	return (
-		<section className="h-screen flex justify-center md:gap-0 items-center flex-col overflow-hidden mx-5">
+		<section className="h-screen flex md:justify-center mt md:m-0 md:gap-0 items-center flex-col overflow-hidden mx-5 overflow-y-hidden">
 			{/* <Background /> */}
-			<div className="container mx-auto ">
+			<div className="container mx-auto mt-10 md:mt-0">
 				<div className="flex flex-col items-center justify-between pb-8">
 					<motion.div
 						className="text-center "
